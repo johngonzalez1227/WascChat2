60d66fccafd77586b2ad7dac58bdfd966da20441
AldenGregory <146675523+AldenGregory@users.noreply.github.com>
2024-12-26 17:36:54 -0800

    Comments added to explain Python backend.

--- flask-server/messaging.py
+++ flask-server/messaging.py
@@ -3,6 +3,15 @@ from google.genai import types
 
 def send_message(frontend_conversation):
 
+    '''
+    This function actually sends a message to Vertex AI and gets its response
+    based on the frontend conversation.
+    Arguments:
+        frontend_conversation: a list of of objects of type types.Part holding
+        information from the frontend json (after it has been formatted).
+    Returns: a string hodling the output message.
+    '''
+
     # The contents array is copied. Then a placeholder response from the model
     # is added because by default because the Python code provided by Vertex AI
     # cuts off the training conversation before the chatbot responds. So, the
@@ -10,6 +19,10 @@ def send_message(frontend_conversation):
     # the frontend conversation is added to the conversation.
     full_conversation = chat_information.contents[:]
     
+    # A placeholder is added to the frontend conversation. The reason for this
+    # is that the last message in the contents object is from the user. But
+    # the first message from the frontend will also be from the user. This 
+    # placeholder ensures that messages alternate between the user and model.
     full_conversation.append(
         types.Content(
             role = "model",
@@ -19,10 +32,15 @@ def send_message(frontend_conversation):
         )
     )
     
+    # The frontend conversation is added to the full conversation.
     full_conversation.extend(frontend_conversation)
 
     output_message = ""
 
+
+    # The generate_content_stream method sends a message to Vertex AI. Then
+    # in this for loop, parts of the response stream are added to
+    # output_message.
     for chunk in chat_information.client.models.generate_content_stream(
         model = chat_information.model,
         contents = full_conversation,
@@ -45,8 +63,16 @@ def format_frontend(frontend):
 
     formatted_frontend = []
 
+    # Loops through each message dictionary in the frontend conversation list.
     for message_dict in frontend:
+
+        # Each element in frontend, message_dict, is a dictionary with keys
+        # "role" and "message".
         formatted_frontend.append(
+
+            # Frontend dictionaries are changed to types.Content objects, since
+            # the function to message Vertex AI requires a list of these
+            # objects.
             types.Content(
                 role = message_dict["role"],
                 parts = [
--- flask-server/server.py
+++ flask-server/server.py
@@ -3,12 +3,27 @@ import messaging
 
 app = Flask(__name__)
 
-# Chat API Route
+# Chat API Route: this endpoint is for HTTP POST requests and it allows
+# conversations from the frontend to be sent to the Vertex AI chatbot then
+# sends back the chatbot's response.
 
 @app.route("/chat", methods = ["POST"])
 
 def chat():
+    '''
+    This function returns messasges based on a frontend conversation.
+    Arguments:
+        None
+    Returns: 
+        A dictionary with one key whose value is the chatbot response.
+    '''
+
+    # This formats incoming JSON to one that can be sent to Vertex AI.
     frontend_conversation = messaging.format_frontend(request.json)
+
+    # A message is sent to Vertex AI using the send_message function and that
+    # message is returned as a dictionary with the key "message" and the
+    # Vertex AI response as its output.
     return {"message": messaging.send_message(frontend_conversation)}
 
 def create_app():
